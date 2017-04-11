5c19ee7ff6ceaccc2cfe0a45f802e09cc6dd2b10
katchs2 <samm.i.amm.katcher@gmail.com>
2017-04-11 15:29:54 -0400

    adding check points 4 and 5

--- checkpoint4.py
+++ checkpoint4.py
@@ -1,5 +1,84 @@
 from pymongo import MongoClient
 client = MongoClient()
 
+# Function to insert data into mongo db
+def insert(db):
+    try:
+        word = raw_input('Enter word ==> ')
+        definition = raw_input('Enter definition ==> ')
+       
+        db.definitions.insert_one(
+               {
+                "definition": definition,
+                "word": word     
+               })
+        print '\nInserted data successfully\n'        
+        
+    except Exception, e:
+        print str(e)
+
+
+# Function to read records from mongo db
+def all_records(db):
+    try:
+        empCol = db.definitions.find()
+        print '\n All data from Database \n'
+        for emp in empCol:
+            print emp
+
+    except Exception, e:
+        print str(e)
+
+
+# Function to read records from mongo db
+def one_record(db):
+    try:
+        emp = db.definitions.find_one()
+        print '\n One data from Database \n'
+        print emp
+
+    except Exception, e:
+        print str(e)
+
+# Function to fetches one specific record from mongo db
+def find_by_name(db,word):
+    try:
+        emp = db.definitions.find({'word': word})
+        print '\n Found ' +word+ ' from Database \n'
+        print word, ":", str(emp)
+        
+    except Exception, e:
+        print str(e)
+
+# Function to fetches one specific record from mongo db
+def find_by_obj_id(db,id):
+    try:
+        emp = db.definitions.find({_id: ObjectId("56fe9e22bad6b23cde07b8ce")})
+        print '\n Found ' +id+ ' from Database \n'
+        print id, ":", str(emp)
+    
+
+    except Exception, e:
+        print str(e)
+
 if __name__ == '__main__':
-    print "Modify me"
+    
+    db = client.csci2963
+    collection = db.definitions
+    
+    ##Fetch all records
+    all_records(db)
+    
+    ##Fetch one record
+    one_record(db)
+    
+
+    
+    ###Fetch a record by object id
+    find_by_obj_id(db, '56fe9e22bad6b23cde07b8b7')
+    
+    ##Insert a new record    
+   # insert(db)
+    
+    ##Fetch a specific record
+    find_by_name(db, 'zone')
--- checkpoint5.py
+++ checkpoint5.py
@@ -1,14 +1,62 @@
 from pymongo import MongoClient
+import random
+import datetime
+
 client = MongoClient()
 
+# Function to read records from mongo db
+def all_records(db):
+    try:
+        empCol = db.definitions.find()
+        print '\n All data from Database \n'
+        for emp in empCol:
+            print emp
 
-def random_word_requester():
-    '''
-    This function should return a random word and its definition and also
-    log in the MongoDB database the timestamp that it was accessed.
-    '''
-    return
+    except Exception, e:
+        print str(e)
+
+
+# Function to fetches one specific record from mongo db
+def find_by_index(db,index):
+    try:
+        emp = db.definitions.find()[index]
+        print '\n Found index from Database \n'
+        print index, ":", str(emp)
+        return emp
+        
+    except Exception, e:
+        print str(e)
+        
+        
+def update_with_date(db, word):
+    db.definitions.update({"word": word}, {'$push' : {"time": datetime.datetime.now()}})
+    print "added time"
+
+###def random_word_requester():
+    ###'''
+    ###This function should return a random word and its definition and also
+    ###log in the MongoDB database the timestamp that it was accessed.
+    ###'''
+    
+    ###rand = random.randint(0, 152)
+    
+    
+    ###return
 
 
 if __name__ == '__main__':
-    print random_word_requester()
+    db = client.csci2963
+    collection = db.definitions
+    size = collection.count()
+    
+    ##Fetch all records
+    all_records(db)
+    
+    ##Fetch one record
+    #rand = random.randint(0, 152)
+    rand = 1
+    randomWord = find_by_index(db, rand)
+    update_with_date(db, randomWord["word"])
+    rword = find_by_index(db, rand)
+    print rword
+    ##print random_word_requester()
